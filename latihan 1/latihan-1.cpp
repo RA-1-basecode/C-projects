fcbd81344c20d6d0a7a1e6cd6aa78742bb7cf059
uniquecoded <ferlanferlani.id@gmail.com>
2022-09-17 02:08:45 +0700

    Jumat 16 September 2022

---
+++ latihan 1/latihan-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    // bagian code yang nantinya di eksekusi
+    cout << "Hello World" << endl;
+    
+    cout << "Nama : Ferlan Ferlani" << endl;
+    cout << "Nim : 220767" << endl;
+    cout << "Jurusan : Teknik Informatika" << endl;
+
+return 0;
+
+}
